2926c87570ab148b012ff1aaaca0e116e5f9bc2d
Levi <Levi@DESKTOP-O0MAH2K>
2022-01-12 14:09:03 +0100

    Add project files.

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book) // json parse miatt kell a paraméterben lévő
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+    }
+}
---
+++ BookAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookAPI.Models;
+
+namespace BookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
